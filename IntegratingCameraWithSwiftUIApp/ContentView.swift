d1146e5e113f55b0cfdded9c109376ab29e65aa1
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-01-21 17:08:29 +0200

    init

--- IntegratingCameraWithSwiftUIApp/ContentView.swift
+++ IntegratingCameraWithSwiftUIApp/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showImagePicker: Bool = false
+    @State private var image: Image? = nil
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Open Camera") {
+                self.showImagePicker = true
+            }
+            .padding()
+            .background(Color.green)
+            .foregroundColor(Color.white)
+            .cornerRadius(10)
+        }.sheet(isPresented: self.$showImagePicker) {
+            PhotoCaptureView(showImagePicker: self.$showImagePicker, image: self.$image)
+        }
     }
 }
 
